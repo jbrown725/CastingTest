541b483e815480e01c462f24da3604ef9b970986
Jordan Brown <jbrown725@gmail.com>
2021-07-15 22:22:32 -0700

    update

--- CastingTest.cpp
+++ CastingTest.cpp
@@ -1,78 +1,86 @@
 #include <iostream>
 #include <iomanip>
 #include <chrono>
-#include <vector>
 #include <random>
-#include <valarray>
 #include <stdio.h>
-#include <memory>
-#include <cstring>
-
 
 using namespace std;
 
 typedef chrono::high_resolution_clock Clock;
 
 int totalEdgeCount_ = 100000000;
-int arrayLength = 10;
+int arrayLength = 2;
+int* r_array;
 
-// highest level class
+// base class
 class IAllVertices {
 public:
     virtual ~IAllVertices() {}
 
-    // trivial test method
-    int test() {
-        //    cout << "IAllVertices" << endl;
-        return 5;
-    };
+    virtual int test1() = 0;
+    virtual int test2() = 0;
 
 };
 
-// middle class level
+// derived class
 class AllVertices : public IAllVertices {
 public:
-    AllVertices();
     virtual ~AllVertices() {};
 };
 
-// lowest class level
+// derived class. the class that gets cast to
 class AllSpikingNeurons : public AllVertices {
 public:
-    AllSpikingNeurons();
     virtual ~AllSpikingNeurons() {};
 
     // trivial test method
-    int test() {
-        // cout << "AllSpikingNeurons" << endl;
-        return 5;
+    virtual int test1() override {
+        return 1;
     };
 
+    // less trivial test method
+    virtual int test2() override {
+        int count = 0;
+        for (int i = 0; i < arrayLength; i++) {
+            count += r_array[i];
+        }
+        return count;
+    }
+};
+
+// lower level derived class
+class AllIFNeurons : public AllSpikingNeurons {
+public:
+    virtual ~AllIFNeurons() {};
+};
+
+// lowest level derived class
+class AllLIFNeurons : public AllIFNeurons {
+public:
+    virtual ~AllLIFNeurons() {};
 };
 
 
-// caller function casts
+
+// caller casting
 void advanceEdge(AllSpikingNeurons* neurons) {
-    int result = neurons->test();
+    int result = neurons->test2();
+}
+// casting is done before passing in
+void advanceEdges(AllSpikingNeurons* vertices) {
+    for (int i = 0; i < totalEdgeCount_; i++) { advanceEdge(vertices); }
 }
 
-void advanceEdgesCasted(AllSpikingNeurons* vertices) {
-    for (int i = 0; i < totalEdgeCount_; i++) {
-        advanceEdge(vertices);
-    }
-}
-
-// callee function casts
+// callee casting
 void advanceEdge(IAllVertices* neurons) {
     AllSpikingNeurons* spNeurons = dynamic_cast<AllSpikingNeurons*>(neurons);
-    int result = spNeurons->test();
+    int result = spNeurons->test2();
+}
+// casting is done each time function is called
+void advanceEdges(IAllVertices* vertices) {
+    for (int i = 0; i < totalEdgeCount_; i++) { advanceEdge(vertices); }
 }
 
-void advanceEdges(IAllVertices* vertices) {
-    for (int i = 0; i < totalEdgeCount_; i++) {
-        advanceEdge(vertices);
-    }
-}
 
 int* makeRandomArray() {
     // fill random array
@@ -86,31 +94,34 @@ int* makeRandomArray() {
 }
 
 
-
 int main() {
-    IAllVertices* parentObject = new IAllVertices();
-    AllSpikingNeurons* castedObject = dynamic_cast<AllSpikingNeurons*>(parentObject);
 
-    int* randomArray = makeRandomArray();
-    // parentObject->randomArray = randomArray;
-    // castedObject->setRandomArray(randomArray);
-    cout << "Running each test " << totalEdgeCount_ << " times" << endl;
+    // instantiate lower level class assigned to base class pointer
+    IAllVertices* parentObject = new AllLIFNeurons();   // 4 levels from base class
+    // IAllVertices* parentObject = new AllSpikingNeurons();  // 2 levels from base class
+    // dynamic cast to class with the defined methods
+    AllSpikingNeurons* castObject = dynamic_cast<AllSpikingNeurons*>(parentObject);
 
+    // populate random array to be used by test methods
+    r_array = makeRandomArray();
+
+    // time the method's execution when callee function casts each time it's called
     auto clockStart = Clock::now();
     advanceEdges(parentObject);
     auto clockEnd = Clock::now();
     auto calleeTime = chrono::duration_cast<chrono::milliseconds>(clockEnd - clockStart).count();
     cout << "Callee casting took " << setw(5) << calleeTime << " ms" << endl;
 
+    // time the method's execution when caller function casts once and passes the cast object
     clockStart = Clock::now();
-    advanceEdgesCasted(castedObject);
+    advanceEdges(castObject);
     clockEnd = Clock::now();
     auto callerTime = chrono::duration_cast<chrono::milliseconds>(clockEnd - clockStart).count();
     cout << "Caller casting took " << setw(5) << callerTime << " ms" << endl;
 
     cout << "Difference in time: " << setw(5) << calleeTime - callerTime << " ms" << endl;
-    cout << "Ratio : "<< (float)calleeTime / callerTime << endl;
-
+    cout << "Casting in the caller method is " << (float)calleeTime / callerTime
+        << " times faster than casting in the callee method" << endl;
 
     return 0;
 }
